d7b1d0964507cf77ca79c6275f08209eeda3fb9e
Mohammad Mofrad <m.hasanzadeh.mofrad@gmail.com>
2018-07-21 01:39:17 -0400

    Add files via upload

---
+++ popup.js
@@ -0,0 +1,45 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+'use strict';
+
+
+let link1 = document.getElementById('link1');
+link1.onclick = function(element)
+{	
+	chrome.tabs.getSelected(null,function(tab)
+	{
+		
+        var tablink = tab.url;
+	    var prelink = "https://host1.rjmusicmedia.com/media/mp3/mp3-256/";
+	    var patt = /([^\/?]+)(?:\?.*)?$/;
+	    var filename = patt.exec(tablink)[1];
+	
+	    var downloadurl = prelink + filename + ".mp3";
+
+        chrome.downloads.download
+		({
+	        url: downloadurl
+        });
+    });
+};
+
+let link2 = document.getElementById('link2');
+link2.onclick = function(element)
+{
+	chrome.tabs.getSelected(null,function(tab)
+	{
+        var tablink = tab.url;
+	    var prelink = "https://host2.rjmusicmedia.com/media/mp3/mp3-256/";
+	    var patt = /([^\/?]+)(?:\?.*)?$/;
+	    var filename = patt.exec(tablink)[1];
+	    var downloadurl = prelink + filename + ".mp3";
+        chrome.downloads.download
+		({
+	        url: downloadurl
+        });
+
+    });
+};
+
